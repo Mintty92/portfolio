941ab950154d285ad37d08e2d2f181f3327a8e70
Andy T. Lincourt <andylincourt@gmail.com>
2016-12-23 15:01:19 -0500

    Only users who created post can edit, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:edit, :update, :show, :delete]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	# Index action to render all posts
 	def index
